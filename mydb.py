e1f4f86ed4a1c9b819d8cd64289d3aad3ae22345
hirenmetaliya <hirenmetaliya2@gmail.com>
2025-01-27 09:35:00 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,17 @@
+# #install mysql using cmd
+# #install mysql-connector
+# #install mysql-connector-python
+
+# import mysql.connector
+
+# dataBase = mysql.connector.connect(
+#     host = 'localhost',
+#     user = 'root',
+#     passwd = '1234',
+#     auth_plugin='mysql_native_password'
+# )
+
+# cursorObject = dataBase.cursor()
+
+# cursorObject.execute("CREATE DATABASE CRM")
+# print("Successfully created!")
